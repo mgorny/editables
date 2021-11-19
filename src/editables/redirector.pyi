100c1a7880b4d129f0f3f835fa6902679c227722
Paul Moore <p.f.moore@gmail.com>
2021-11-19 13:29:26 +0000

    RedirectingFinder methods are classmethods (pyi file)

--- src/editables/redirector.pyi
+++ src/editables/redirector.pyi
@@ -6,12 +6,15 @@ from typing import Sequence
 class RedirectingFinder:
     _redirections: dict[str, str]
 
+    @classmethod
     def map_module(cls, name: str, path: str) -> None:
         ...
 
     # Match the ABC definition from typeshed
+    @classmethod
     def find_spec(cls, fullname: str, path: Sequence[bytes|str] | None = ..., target: types.ModuleType | None = ...) -> ModuleSpec | None:
         ...
 
+    @classmethod
     def install(cls) -> None:
         ...
